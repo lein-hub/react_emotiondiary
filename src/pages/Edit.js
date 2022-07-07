562ee9436da1973fbffd68eff120f7ff31f0bb64
lein-hub <75424893+lein-hub@users.noreply.github.com>
2022-07-07 10:30:00 +0900

    useSearchParams & navigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 
 function Edit() {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get('id');
+  const mode = searchParams.get('mode');
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 글 수정 페이지 입니다.</p>
+      <button onClick={()=>setSearchParams({who: "winterlood"})}>QS 바꾸기</button>
+      <button onClick={()=>navigate("/")}>홈으로</button>
+      <button onClick={()=>navigate(-1)}>뒤로가기</button>
     </div>
   )
 }
