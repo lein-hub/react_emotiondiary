7a60de51dc21d7199c5e70c9217b7468a6f07d1d
lein-hub <75424893+lein-hub@users.noreply.github.com>
2022-07-07 15:32:47 +0900

    MyButton

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import Home from './pages/Home.js';
 import New from './pages/New.js';
 import Edit from './pages/Edit.js';
 import Diary from './pages/Diary.js';
-import RouteTest from './components/RouteTest';
+import MyButton from './components/MyButton';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+
+        <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')} type={"positive"}></MyButton>
+        <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')} type={"negative"}></MyButton>
+        <MyButton text={'버튼'} onClick={()=>alert('버튼 클릭')}></MyButton>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest></RouteTest> 
       </div>
     </BrowserRouter>
   );
